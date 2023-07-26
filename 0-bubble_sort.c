383df0d19d6388d65bf3e6c6740d54e4ddd07efb
Olamilekan <akintolaolamilekan51@gmail.com>
2023-07-26 10:26:23 +0100

    updated bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -22,7 +22,7 @@ void bubble_sort(int *array, size_t size)
 		for (j = 0; j < size - i - 1; j++)
 		{
 			/*compare element values*/
-			if (array[j] >= array[j + 1])
+			if (array[j] > array[j + 1])
 			{
 				swapped = 1;
 				temp = array[j];
